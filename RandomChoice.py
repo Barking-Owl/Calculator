964c54397bbdb8a19f48d3a0d306f9a779430bf4
A.N <77659600+Barking-Owl@users.noreply.github.com>
2021-08-16 20:05:33 -0500

    Update RandomChoice.py
    Adds another component to the program to simulate a dice roll, will fill later.

--- RandomChoice.py
+++ RandomChoice.py
@@ -13,4 +13,16 @@ s = random.choice(things)
 print("Your list consisted of: ")
 for r in range(len((things))):
     print (things[r])
-print("We chose: " + s)
+print("The chosen option was: " + s)
+
+#This code is also used in the Hangman project in this Repository. 
+
+dice = input("Input the amount of sides your dice has: ") #Amount of sides of the dice. Should accept ONLY Integers.
+
+#Any modifications? Could take from the Calculator program
+
+#Results of dice roll
+print("Your dice roll landed on: " + dice)
+      
+#Rolling another dice for advantage/disadvantage?
+
